d6b459818ba975fc21850df2397c84ccb1e8ce2a
Prj-geek <2727priyanshu@gmail.com>
2026-02-25 19:54:59 +0530

    Implement Spotify token retrieval in song recommendation endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,5 @@
+import requests
+import base64
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -13,7 +15,33 @@ class SongRequest(BaseModel):
 
 @app.post("/recommend")
 def recommend_song(request: SongRequest):
+  token = get_spotify_token()
+
   return {
     "input_song": request.song_name,
-    "recommended_song": "Mock Song"
+    "spotify_token_preview": token[:20]
   }
+
+def get_spotify_token():
+    client_id = "8969f7ba5dde4581ba4121d123298775"
+    client_secret = "0ec67d1c9258433fa270183248207d91"
+
+    auth_string = f"{client_id}:{client_secret}"
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = base64.b64encode(auth_bytes).decode("utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+
+    headers = {
+        "Authorization": f"Basic {auth_base64}",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    data = {
+        "grant_type": "client_credentials"
+    }
+
+    result = requests.post(url, headers=headers, data=data)
+    json_result = result.json()
+
+    return json_result["access_token"]
